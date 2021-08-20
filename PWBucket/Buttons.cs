1a5ec95bbcd1d36976164eedd13fe7ce2d7efd11
P-Dubinski <89077732+P-Dubinski@users.noreply.github.com>
2021-08-20 10:44:03 +0200

    Update 0.1
    Anstatt einer MSSQL Datenbank wird nun das interne C# Visual Studio Datenbank Tool verwendet.

--- PWBucket/Buttons.cs
+++ PWBucket/Buttons.cs
@@ -20,37 +20,7 @@ namespace PWBucket
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String str;
-            SqlConnection myConn = new SqlConnection("Server=localhost;Integrated security=SSPI;database=master");
-
-            str = "CREATE DATABASE MyDatabase ON PRIMARY " +
-             "(NAME = MyDatabase_Data, " +
-             "FILENAME = 'C:\\PWBucket\\MyDatabaseData.mdf', " +
-             "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%)" +
-             "LOG ON (NAME = MyDatabase_Log, " +
-             "FILENAME = 'C:\\PWBucket\\MyDatabaseLog.ldf', " +
-             "SIZE = 1MB, " +
-             "MAXSIZE = 5MB, " +
-             "FILEGROWTH = 10%)";
-
-            SqlCommand myCommand = new SqlCommand(str, myConn);
-            try
-            {
-                myConn.Open();
-                myCommand.ExecuteNonQuery();
-                MessageBox.Show("DataBase is Created Successfully", "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            catch (System.Exception ex)
-            {
-                MessageBox.Show(ex.ToString(), "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            finally
-            {
-                if (myConn.State == ConnectionState.Open)
-                {
-                    myConn.Close();
-                }
-            }
+            
         }
     }
 }
